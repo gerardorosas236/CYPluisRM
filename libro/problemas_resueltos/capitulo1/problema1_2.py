55381a40826760ca7879c49d0f15671faa519119
gerardorosas236 <gerardorosas236@gmail.com>
2019-09-19 17:30:50 -0500

    Problema del 1.1 al 1.3 Funcionando

---
+++ libro/problemas_resueltos/capitulo1/problema1_2.py
@@ -0,0 +1,4 @@
+BASE = float(input("Introduce la base del Triangulo: "))
+ALTU = float(input("Introduce la Altura del Triangulo: "))
+SUP = (BASE*ALTU)/2
+print(f"La superficie del triangulo es: {SUP} dado que la altura es: {ALTU} y la base: {BASE}")
---
+++ libro/problemas_resueltos/capitulo1/problema1_3.py
@@ -0,0 +1,6 @@
+NOM = input("Ingresa el nombre del Dinosurio: ")
+PES = float(input("Cual es el peso del dinosaurio en toneladas: "))
+LON = float(input("Cual es la longitud del dinosaurioen pies: "))
+PESKIL = PES*1000
+LONMET = LON*0.3047
+print(f"El dinosaurio es: {NOM} el peso de {NOM} en kilos es: {PESKIL} y el tamaño de {NOM} en metros es: {LONMET}")
