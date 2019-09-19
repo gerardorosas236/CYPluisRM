1d800f767a1b6729d61b77f38c763ce71099049a
gerardorosas236 <gerardorosas236@gmail.com>
2019-09-18 21:41:27 -0500

    Problema 1.11 Funcionando

---
+++ libro/ejemplo1_11.py
@@ -0,0 +1,6 @@
+A = int(input("Dame un numero entero: "))
+B = int(input("Dame un segundo numero entero: "))
+C = int(input("Dame un tercer numero entero: "))
+D = int(input("Dame un cuarto numero entero: "))
+print(f"El numero generado del ultimo al primero es: {D} {C} {B} {A}")
+
