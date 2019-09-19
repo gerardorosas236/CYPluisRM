b9f817e585e99cc0aac9cf9e06614dfef33308b5
gerardorosas236 <gerardorosas236@gmail.com>
2019-09-18 22:01:35 -0500

    Problema 1.10 Funcionando

--- libro/ejemplo1_10.py
+++ libro/ejemplo1_10.py
@@ -1,6 +1,6 @@
-BASE = float(input("Introduce el Valor de la Base del Rectangulo"))
-ALTU = float(input("Introduce el Valor de la Altura del Rectangulo"))
+BASE = float(input("Introduce el valor de la base del Recatangulo: "))
+ALTU = float(input("Intuduce el valor de la Altura del Rectangulo: "))
 SUP = BASE * ALTU
 PER = 2*(BASE + ALTU)
-print (f "La Superficie del Rectangulo es : {SUP}")
-print (f "El Perimetro del Rectangulo es : {PER}")
+print (f"La Superficie del Rectangulo es : { SUP }")
+print (f"El Perimetro del Rectangulo es : {PER }")
