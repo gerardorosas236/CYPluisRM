180a0b4ad1ef081b1ae57ee218a53ee9c3c535b1
gerardorosas236 <gerardorosas236@gmail.com>
2019-09-20 21:54:50 -0500

    Problema 1.8 Funcionando

---
+++ libro/problemas_resueltos/capitulo1/problema1_8.py
@@ -0,0 +1,8 @@
+print("Programa Que Calcule La Distancia Entre Dos Puntos")
+X1 = float(input("Ingresa las Coordenadas del Punto 1 X1: "))
+Y1 = float(input("Ingresa las Coordenadas del Punto 1 Y1: "))
+X2 = float(input("Ingresa las Coordenadas del Punto 2 X2: "))
+Y2 = float(input("Ingresa las Coordenadas del Punto 2 Y2: "))
+DIS = ((X1-X2) ** 2 + (Y1-Y2) ** 2) ** 0.5
+print(f"La Distancia Entre Los Dos Puntos Dados Es: {DIS} ")
+
