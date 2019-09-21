12d912251372ff799ae0a647bcdccc694bc4e901
gerardorosas236 <gerardorosas236@gmail.com>
2019-09-20 21:26:44 -0500

    Problema 1.6 Funcionando

---
+++ libro/problemas_resueltos/capitulo1/problema1_6.py
@@ -0,0 +1,4 @@
+print("Programa Que Calcula El Numero De Segundos Que Hay En Un Determinado Numero De Dias")
+DIAS = int(input("Ingresa el Numero de Dias a Calcular: "))
+SEG = DIAS * 24 * 60 * 60
+print(f"En {DIAS} dias hay {SEG} segundos") 
