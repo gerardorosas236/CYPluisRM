77397147fccb5c6ec6bde7efa199edc22bce30ae
gerardorosas236 <gerardorosas236@gmail.com>
2019-09-20 20:32:10 -0500

    Ejercicio 1.13 Funcionando

---
+++ libro/ejemplo1_13.py
@@ -0,0 +1,9 @@
+MAT = int(input("Ingresa tu Matricula de alumno: "))
+CAL1 = float(input("Ingresa tu Primera Calificacion: "))
+CAL2 = float(input("Ingresa tu Segunda Calificacion: "))
+CAL3 = float(input("Ingresa tu Tercera Calificacion: "))
+CAL4 = float(input("Ingresa tu Cuarta Calificacion: "))
+CAL5 = float(input("Ingresa tu Quinta Calificacion: "))
+PROM = (CAL1 + CAL2 + CAL3 + CAL4 + CAL5)/5
+print(f"La Matricula con Numero: {MAT} Obtuvo el Promedio de: {PROM}") 
+
