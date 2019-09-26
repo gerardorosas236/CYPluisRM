e2328dced375c932bbae6aba05bb0fddb81b4465
gerardorosas236 <gerardorosas236@gmail.com>
2019-09-25 22:43:47 -0500

    Ejemplo 2.3 Funcionando

---
+++ libro/ejemplo2_3.py
@@ -0,0 +1,6 @@
+CAL = float(input("Ingresa Tu Calificacion Obtenida En El Examen: "))
+if CAL>=8:
+    print("Calificacion Aprobatoria")
+else:
+    print("Calificacion Reprobatoria")
+print("Fin Del Programa....")
