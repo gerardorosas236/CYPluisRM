dff9988a84165ddf6f76c060dce59633166ff2b7
gerardorosas236 <gerardorosas236@gmail.com>
2019-09-28 20:58:06 -0500

    Problema 2.1 Funcionando

---
+++ libro/problemas_resueltos/capitulo2/problema2_1.py
@@ -0,0 +1,5 @@
+N = int(input("Ingrese el Numero de Sonidos Emitidos por Minuto del Grillito: " ))
+if N>0:
+    T = N/4+40
+    print(f"La Temperatura en Grados Fahrenheit es de: {T}")
+print("Fin del Programa....")
