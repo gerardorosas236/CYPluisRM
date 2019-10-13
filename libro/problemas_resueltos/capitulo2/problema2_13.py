36951366459e0464d84b57e0c0da1f84149447c9
gerardorosas236 <gerardorosas236@gmail.com>
2019-10-13 14:51:08 -0500

    Problema 2.13 Funcionando

---
+++ libro/problemas_resueltos/capitulo2/problema2_13.py
@@ -0,0 +1,13 @@
+MAT = int(input("Ingresa Tu Matricula De Alumno: "))
+CARR = str(input("Ingresa La Carrera En La Que Estas Inscrito: "))
+SEM = int(input("Ingresa El Semestre En el Que Estas Inscrito: "))
+PROM = float(input("Ingresa Tu Promedio: "))
+if CARR=="economia":
+    if SEM>=6 and PROM>=8.8:
+        print(f"Matricula {MAT}, Carrera {CARR}, Aceptado")
+    elif CARR=="computacion":
+        if  SEM>6 and PROM>8.5:
+            print(f"Matricula {MAT}, Carrera {CARR}, Aceptado")
+        elif CARR=="contabildad":
+            if SEM>5 and PROM>8.5:
+                print(f"Matricula {MAT}, Carrera {CARR}, Aceptado")
