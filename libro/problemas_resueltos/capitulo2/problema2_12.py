f5a76be4d277df821d617e276642dc62ee6cccd4
gerardorosas236 <gerardorosas236@gmail.com>
2019-10-13 14:15:43 -0500

    Problema 2.12 Funcionando

---
+++ libro/problemas_resueltos/capitulo2/problema2_12.py
@@ -0,0 +1,18 @@
+SUE = float(input("Ingrese Su Sueldo: "))
+CATE = int(input("Ingrese La Categoria A la Que Pertenece: "))
+HE = int(input("Ingrese Sus Horas Extras Trabajadas: "))
+if CATE==1:
+    PHE=30
+elif CATE==2:
+    PHE=38
+elif CATE==3:
+    PHE=50
+elif CATE==4:
+    PHE=70
+else:
+    PHE=0
+if HE>30:
+    NSUE = SUE+30*PHE
+else:
+    NSUE = SUE+HE*PHE
+    print(f"Su Sueldo Total Con Horas Extras Trabajadas Es De: ${NSUE}")
