b1d371bd60eeb9a649530a76362ec62ea5243ac6
gerardorosas236 <gerardorosas236@gmail.com>
2019-11-14 09:00:14 -0600

    Tarea

---
+++ redlines.py
@@ -0,0 +1,26 @@
+archivo =  abierto ( " numeros.txt " , " rt " )
+print ( dir (archivo))
+
+numeros1 = archivo.read ()
+print (numeros1)
+print (numeros1.split ( ' \ n ' ))
+lista_num = []
+for line in numeros1.split ( ' \ n ' ):
+    for numero in linea.split ( ' , ' ):
+        lista_num.append ( int (numero))
+print (lista_num)
+lista_num.sort ()
+print ( f " \n Lista ordenada: { lista_num } \n ") 
+
+print ( f " El mayor es: { lista_num [ - 1 ] } y el menor es: { lista_num [ 0 ] } " )
+archivo.close ()
+
+archivo = abierto ( " numeros.txt " , " rt " )
+numeros2 = archivo.readlines ()
+print (numeros2)
+archivo.close ()
+
+archivo = abierto ( " numeros.txt " , " rt " )
+numeros2 = archivo.readline ()
+print (numeros2)
+archivo.close ()
